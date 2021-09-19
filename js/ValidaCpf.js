a87f9564b737797e37e62881399115af05d63707
jhonozorio <jleandro.dev@gmail.com>
2021-09-19 10:17:39 -0300

    update code

--- js/ValidaCpf.js
+++ js/ValidaCpf.js
@@ -1,63 +1,51 @@
-
-
-// let campo = document.getElementById('campo');
-
-// campo.addEventListener('focus', () => {
-//    campo.style.borderColor = "#3b5998";
-// });
-
-
-function validaCpf(cpfEnviado) {
+class ValidaCpf {
+  constructor(cpfEnviado){
   Object.defineProperty(this, 'cpfLimpo', {
+    writable: false,
     enumerable: true,
-    get: function(){
-      return cpfEnviado.replace(/\D+/g, '')
-    }
-  });
+    configurable: false,
+    value: cpfEnviado.replace(/\D+/g, '')
+  })
 };
 
-validaCpf.prototype.valida = function(){
-  if(typeof this.cpfLimpo === 'undefined') return false;
-  if(this.cpfLimpo.length !== 11) return false;
-  if(this.isNumRepeat()) return false;
-
-  const cpfParcial = this.cpfLimpo.slice(0, -2);
-  const digito1 = this.criaDigito(cpfParcial);
-  const digito2 = this.criaDigito(cpfParcial + digito1);
-
-  const novoCpf = cpfParcial + digito1 + digito2;
-
-  return novoCpf === this.cpfLimpo;
-};
-
-validaCpf.prototype.isNumRepeat = function(){
-  const numRepeat = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
-  return numRepeat === this.cpfLimpo;
+geraNovoCpf() {
+  const semDigitos = this.cpfLimpo.slice(0, -2);
+  const digito1 = this.geraDigito(semDigitos);
+  const digito2 = this.geraDigito(semDigitos + digito1);
+  this.novoCpf = semDigitos + digito1 + digito2;
 }
 
+geraDigito(semDigitos) {
+  let total = 0;
+  let reverso = semDigitos.length + 1;
 
-validaCpf.prototype.criaDigito = function(cpfParcial) {
-  const cpfArray = Array.from(cpfParcial);
-  let regressivo = cpfArray.length + 1;
-  let total = cpfArray.reduce((ac, val) => {
-    ac += (regressivo * Number(val));
-    regressivo--;
-    return ac;
-  }, 0);
+  for(let stringNum of semDigitos) {
+    total += reverso * Number(stringNum);
+    reverso--;
+  }
   const digito = 11 - (total % 11);
-  return digito > 9 ? '0' : String(digito);
+  return digito <= 9 ? String(digito) : '0';
+}
+
+validar() {
+  if(! this.cpfLimpo) return false;
+  if(typeof this.cpfLimpo !== 'string') return false;
+  if(this.cpfLimpo.length !== 11) return false;
+  if(this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo) return false;
+  this.geraNovoCpf();
+  return this.novoCpf === this.cpfLimpo;
+}
+
 };
 
-let resultado = document.getElementById('result');
 
-
-
-const validacao = function(){
+const validaCpf = () => {
   let cpfRecebido = document.getElementById("campo").value;
+  let resultado = document.getElementById('result');
 
-  const cpf = new validaCpf(cpfRecebido);
+  const cpf = new ValidaCpf(cpfRecebido);
 
- if(cpf.valida()){
+ if(cpf.validar()){
   resultado.innerHTML = 'CPF Válido!';
   resultado.style.color = '#2ecc71';
 
